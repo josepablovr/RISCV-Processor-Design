1f61551efa3b88e2aad4521e9ecee168b3d6a008
josepablovr <josepablo2106@gmail.com>
2021-10-26 17:47:29 -0600

    Correccion Sumador

--- Sumador.v
+++ Sumador.v
@@ -18,13 +18,42 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Sumador(A, B, S);
-	 input signed [31:0] A; //Registro 1 de la suma
-	 input signed [31:0] B; //Registro 2 o Inmediato de la suma 
-	 output reg signed [31:0] S;
+
+/* 
+Funcionalidad: este módulo se encarga de hacer la operación de suma de 2 valores
+que son ingresados en la ALU, ya sean registro-registro o registro-inmediato. 
+
+Entradas:
+
+A: Entrada del primer valor a sumar almacenado en registro.
+B: Entrada del segundo valor a sumar almacenado en registro o como inmediato.
+
+Salidas:
+
+S: Resultado de la suma.
+
+Razón de uso:
+Este módulo se usa como parte del conjunto de operaciones de la ALU, las
+cuales van a ser seleccionadas por el seleccionador para ejecutarse, busca
+realzair una suma para operaciones de add o para buscar la nueva dirección
+*/
+
+module Sumador(
+
+	 input signed [31:0] A, //Registro 1 de la suma
+	 input signed [31:0] B, //Registro 2 o Inmediato de la suma 
+	 output reg signed [31:0] S //Resultado de la suma
+    );
+	 
+	 reg signed [32:0] Sum;
 	 
 	 always @*
 		S <= A + B;
+		
+	
+	 
+
 
 endmodule
+
 
