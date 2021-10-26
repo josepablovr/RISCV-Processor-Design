40349fef6b07a1175434f1198534665e9beb7955
Joshua Corvera <jcorvera10@hotmail.com>
2021-10-25 23:10:52 -0600

    Update AND.v

--- AND.v
+++ AND.v
@@ -18,6 +18,25 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Funcionalidad: este módulo se encarga de hacer la operación de AND de 2 valores
+que son ingresados en la ALU 
+
+Entradas:
+
+A: Entrada de la ALU, entrada número 1
+B: Entrada de la ALU, entrada número 2
+
+Salidas:
+
+R: Resultado de la operación AND
+
+Razón de uso:
+Este módulo se usa como parte del conjunto de operaciones de la ALU, las
+cuales van a ser seleccionadas por el seleccionador para ejecutarse.
+*/
+
 module AND(
     input [31:0] A, //Entrada A de la AND
     input [31:0] B, //Entrada B de la AND
