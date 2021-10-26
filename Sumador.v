516fa0714ab9c406c99653c0b7349a00a860a024
Joshua Corvera <jcorvera10@hotmail.com>
2021-10-25 23:42:50 -0600

    Update Sumador.v

--- Sumador.v
+++ Sumador.v
@@ -18,6 +18,26 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Funcionalidad: este módulo se encarga de hacer la operación de suma de 2 valores
+que son ingresados en la ALU, ya sean registro-registro o registro-inmediato. 
+
+Entradas:
+
+A: Entrada del primer valor a sumar almacenado en registro.
+B: Entrada del segundo valor a sumar almacenado en registro o como inmediato.
+
+Salidas:
+
+S: Resultado de la suma.
+
+Razón de uso:
+Este módulo se usa como parte del conjunto de operaciones de la ALU, las
+cuales van a ser seleccionadas por el seleccionador para ejecutarse, busca
+realzair una suma para operaciones de add o para buscar la nueva dirección
+*/
+
 module Sumador(
 
 	 input signed [31:0] A, //Registro 1 de la suma
