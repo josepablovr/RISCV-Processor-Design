61ee1ebaa49cc824d294d7b2be427dc855df58ab
Charlie2064 <88814436+Charlie2064@users.noreply.github.com>
2021-11-17 22:20:43 -0600

    Update PC.v

--- PC.v
+++ PC.v
@@ -18,6 +18,21 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+/*
+Funcionalidad: este simple pero indispensable módulo permite acceder a la memoria de instrucciones.
+
+Entradas: 
+clk: señal de reloj
+rst: señal de reset
+PC_in: dato a almacenar durante el ciclo de reloj
+
+Salidas:
+PC_out: valor 'actual' del FF
+
+Razón de uso: este Flip Flop permite almacenar valores de entrada y mantenerlos durante un determinado tiempo, lo cual es útil para la selección de instrucciones.
+
+*/
 module PC(clk, rst, PC_in, PC_out);
 	input clk;	
 	input rst;	
